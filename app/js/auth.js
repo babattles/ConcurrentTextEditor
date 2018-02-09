f3dd04f5bd8a7cf0bc640c405dfe3f3a739e9f08
babattles <babattles@gmail.com>
2018-02-09 14:08:48 -0500

    Added Login/Register Modal Functionality + Logout Functionality
    Additionally:
    * Restructured files
    * Added CSS style sheets (blank)

---
+++ app/js/auth.js
@@ -0,0 +1,61 @@
+/**
+ * Created by bbattles on 2/8/2018
+ * Login & Register Functionality
+ * */
+'use strict';
+const {ipcRenderer} = require('electron');
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAr4i-0nzmJi9x2bwyJXqQPjsPJfkeN0V0",
+    authDomain: "hivetext-dcadf.firebaseapp.com",
+    databaseURL: "https://hivetext-dcadf.firebaseio.com",
+    projectId: "hivetext-dcadf",
+    storageBucket: "hivetext-dcadf.appspot.com",
+    messagingSenderId: "254482798300"
+};
+firebase.initializeApp(config);
+
+var loginBtn = document.getElementById("loginBtn");
+var registerBtn = document.getElementById("registerBtn");
+var closeBtn = document.getElementById("closeBtn");
+  
+// Login Button was clicked 
+loginBtn.addEventListener('click', function(e) {
+    var loginEmail = document.getElementById('email').value;
+    var loginPassword = document.getElementById('password').value;
+  
+    // Sign in with email & password
+    firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword).then(function() {
+        // Close auth window
+        ipcRenderer.send('close-auth-window', 'logged');
+    }).catch(function(error) {
+        if (error != null) {
+            alert("ERROR LOGGING IN: Please Enter Correct Credentials");
+            console.log(error.message);
+            return;
+        }
+    });
+});
+  
+// Register Button was clicked
+registerBtn.addEventListener("click", function() {
+    var emailField = document.getElementById('email').value;
+    var passwordField = document.getElementById('password').value;
+  
+    firebase.auth().createUserWithEmailAndPassword(emailField, passwordField).then(function() {
+        alert('User Registered... Logging in...');
+        ipcRenderer.send('close-auth-window', 'logged');
+    }).catch(function(error) {
+        if (error != null) {
+            alert("ERROR REGISTERING ACCOUNT");
+            console.log(error.message);
+            return;
+        }
+    });
+});
+
+// Close Button was clicked
+closeBtn.addEventListener("click", function() {
+    ipcRenderer.send('close-auth-window', 'ping');
+});
---
+++ main.js
@@ -0,0 +1,103 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
+const {ipcMain} = require('electron');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win = null; 
+let authWindow = null;
+
+/* Create the browser window. */
+function createWindow () {
+  win = new BrowserWindow({
+    //frame: false,
+    //resizeable: false,
+    width: 1000, 
+    height: 900,
+  });
+
+  // and load the index.html of the app.
+  
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, '/app/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  /**
+  * Uncomment line below to enable developer tools when opening this window
+  */
+  //win.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+});
+
+// Listen for message to open the Auth Window (auth.html)
+ipcMain.on('open-auth-window', (event, arg) => {
+    if (authWindow) {
+        return;
+    }
+
+    authWindow = new BrowserWindow({
+        parent: win, 
+        modal: true, 
+        show: false
+    });
+
+    authWindow.loadURL('file://' + __dirname + '/app/auth.html');
+
+    authWindow.once('ready-to-show', () => {
+        authWindow.show();
+    });
+
+    /**
+     * Uncomment line below to enable developer tools when opening this window
+     */
+    //authWindow.webContents.openDevTools();
+
+    authWindow.on('closed', function () {
+        authWindow = null;
+    });
+});
+
+// Listen for message to close auth window
+ipcMain.on('close-auth-window', (event, arg) => {
+    if (authWindow) {
+        authWindow.close();
+    }
+
+    if (arg == 'logged') {
+      // do something on login
+    }
+});

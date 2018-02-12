1fe6e7718649a5aee2d0d37d19bf8b5a6a94744e
Alex Geier <alexg321@gmail.com>
2018-02-11 19:33:56 -0500

    Temp

--- app/js/userSettings.js
+++ app/js/userSettings.js
@@ -6,18 +6,43 @@ document.querySelector('#toggle-user-settings').onclick = function() {
 changeName = function(name) {
     // console.log(name);
     alert("(TEMP)Changed name to " + name);
+    // var user = firebase.auth().currentUser;
+
+    // user.updateProfile({
+    //     displayName: "Jane Q. User",
+    //     photoURL: "https://example.com/jane-q-user/profile.jpg"
+    // }).then(function() {
+    //     // Update successful.
+    // }).catch(function(error) {
+    //     // An error happened.
+    // });
     clearChangeName();
 }
 
 changeUsername = function(username) {
     // console.log(username);
     alert("(TEMP)Changed username to " + username);
+    // var user = firebase.auth().currentUser;
+
+    // user.updateEmail("user@example.com").then(function() {
+    //     // Update successful.
+    // }).catch(function(error) {
+    //     // An error happened.
+    // });
     clearChangeUsername();
 }
 
 changePassword = function(password) {
     // console.log(password);
     alert("(TEMP)Changed password to " + password);
+    // var user = firebase.auth().currentUser;
+    // var newPassword = "asdf";
+    // //salt and hash ppassword;
+    // user.updatePassword(newPassword).then(function() {
+    //     // Update successful.
+    // }, function(error) {
+    //     // An error happened.
+    // });
     clearChangePassword();
     //need to hash + salt before sending to server
 }
@@ -27,6 +52,17 @@ changePassword = function(password) {
 //     alert("(TEMP)Logged out");
 //     clearUserSettings();
 // }
+
+sendPasswordResetEmail = function() {
+    var auth = firebase.auth();
+    // var user = firebase.auth().currentUser
+    var emailAddress = "alexjgeier@gmail.com";
+    auth.sendPasswordResetEmail(emailAddress).then(function() {
+        // Email sent.
+    }).catch(function(error) {
+        // An error happened.
+    });
+}
 
 clearChangeName = function() {
     document.querySelector("#change-name-input").value = null;
@@ -61,6 +97,12 @@ document.querySelector('#change-username-submit').onclick = function() {
 document.querySelector('#change-password-submit').onclick = function() {
     console.log("User attempt change password");
     changePassword(document.querySelector("#change-password-input").value);
+};
+
+document.querySelector('#reset-password').onclick = function() {
+    console.log("User attempt reset password");
+    alert();
+    sendPasswordResetEmail();
 };
 
 // document.querySelector('#logout-button').onclick = function() {

52cfff04a4484fd8e446914bf0fc348aaa84dc66
Alex Geier <alexjgeier@gmail.com>
2018-02-15 16:59:44 -0500

    Fixed bug causing files to show up on one line for mac users

--- app/js/localFile.js
+++ app/js/localFile.js
@@ -1,67 +1,65 @@
 const electron = require('electron');
 const dialog = require('electron').remote.dialog
+const fs = require('fs');
 
-var PythonShell = require('python-shell');
-var openedFile = document.getElementById('openedFile');
+var openedFile = document.getElementById('openFileBtn');
 var editor = document.getElementById('temptextarea');
 var saveFileBtn = document.getElementById('saveFileBtn');
 var path = '';
+var fileContents = '';
+var currentFileName = '';
 
-openedFile.onchange = function() {
-    // TODO this may not work correctly if multiple files are opened.(ie ones is opened, closed and then another one is opened.)
-    var file = document.getElementById('openedFile').files[0];
-    var path = file.path;
+// openedFile.onchange = function() {
+openFileBtn.addEventListener('click', function() {
+    dialog.showOpenDialog((fileNames) => {
+        path = fileNames[0];
+        currentFileName = fileNames[0].substring(fileNames[0].lastIndexOf("\\") + 1, fileNames[0].length);
+        fs.readFile(path, 'utf-8', (err, data) => {
+            if (err) {
+                alert("An error ocurred reading the file :" + err.message);
+                return;
+            }
 
-    var currentFileName = file.name;
-    var currentUserName = '';
+            // show the text in the editor
+            fileContents = data;
+            console.log(fileContents);
+            editor.textContent = data;
 
-    var options = {
-        args: [path]
-    };
-
-    PythonShell.run('./app/python_scripts/openFile.py', options, function(err, results) {
-        if (err) throw err;
-        var fileContents = '';
-        for (i = 0; i < results.length; i++) {
-            fileContents += results[i];
-        }
-        fileContents = fileContents.substring(0, fileContents.length - 1);
-
-        // Add file to user's account
-        var user = firebase.auth().currentUser;
-        if (user) {
-            // user is logged in
-            var userRef = firebase.database().ref().child("users").child(user.uid);
-            // get the user's username
-            userRef.child("username").once("value").then(function(snapshot) {
-                currentUserName = snapshot.val();
-                var fileList = firebase.database().ref().child('files');
-                var newFile = fileList.push(); // generate a new fileID
-                newFile.set({
-                    'fileName': currentFileName,
-                    'fileContents': fileContents,
+            // Add file to user's account
+            var user = firebase.auth().currentUser;
+            if (user) {
+                // user is logged in
+                var userRef = firebase.database().ref().child("users").child(user.uid);
+                // get the user's username
+                userRef.child("username").once("value").then(function(snapshot) {
+                    currentUserName = snapshot.val();
+                    var fileList = firebase.database().ref().child('files');
+                    var newFile = fileList.push(); // generate a new fileID
+                    newFile.set({
+                        'fileName': currentFileName,
+                        'fileContents': fileContents
+                    });
+                    // add user to file's userList
+                    newFile.child('userList').child(user.uid).set({ 'username': currentUserName });
+                    // add fileID to user's fileList
+                    userRef.child('fileList').child(newFile.key).set({ 'fileName': currentFileName });
                 });
-                // add user to file's userList
-                newFile.child('userList').child(user.uid).set({'username' : currentUserName});
-                // add fileID to user's fileList
-                userRef.child('fileList').child(newFile.key).set({'fileName' : currentFileName});
-            });
-        }
-
-        // show the text in the editor
-        editor.textContent = fileContents;
-    });
-};
-
-saveFileBtn.addEventListener('click', function() {
-    if (path === '') {
-        alert("You must open a file first.");
-        return;
-    }
-    var options = {
-        args: [path, editor.value]
-    };
-    PythonShell.run('./app/python_scripts/saveFile.py', options, function(err, results) {
-        if (err) throw err;
+            }
+        });
     });
 });
+
+saveFileBtn.addEventListener('click', function() {
+    if (path) {
+        fs.writeFile(path, editor.value, function(err) {
+            if (err) {
+                alert("An error ocurred updating the file" + err.message);
+                console.log(err);
+                return;
+            }
+            alert("The file has been succesfully saved");
+        });
+    } else {
+        alert("Please select a file first");
+    }
+}, false);

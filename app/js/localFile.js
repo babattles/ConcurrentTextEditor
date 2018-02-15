edf6e69305df3a7ef553a5a98785aeb40bada63f
Alex Geier <alexjgeier@gmail.com>
2018-02-15 16:59:44 -0500

    new open and file save behavior

--- app/js/localFile.js
+++ app/js/localFile.js
@@ -1,67 +1,63 @@
 const electron = require('electron');
 const dialog = require('electron').remote.dialog
+const fs = require('fs');
 
-var PythonShell = require('python-shell');
-var openedFile = document.getElementById('openedFile');
+var openedFile = document.getElementById('openFileBtn');
 var editor = document.getElementById('temptextarea');
 var saveFileBtn = document.getElementById('saveFileBtn');
 var path = '';
 
-openedFile.onchange = function() {
-    // TODO this may not work correctly if multiple files are opened.(ie ones is opened, closed and then another one is opened.)
-    var file = document.getElementById('openedFile').files[0];
-    var path = file.path;
+// openedFile.onchange = function() {
+openFileBtn.addEventListener('click', function() {
+    var fileContents = '';
+    dialog.showOpenDialog((fileNames) => {
+        path = fileNames[0];
+        fs.readFile(path, 'utf-8', (err, data) => {
+            if (err) {
+                alert("An error ocurred reading the file :" + err.message);
+                return;
+            }
 
-    var currentFileName = file.name;
-    var currentUserName = '';
-
-    var options = {
-        args: [path]
-    };
-
-    PythonShell.run('./app/python_scripts/openFile.py', options, function(err, results) {
-        if (err) throw err;
-        var fileContents = '';
-        for (i = 0; i < results.length; i++) {
-            fileContents += results[i];
-        }
-        fileContents = fileContents.substring(0, fileContents.length - 1);
-
-        // Add file to user's account
-        var user = firebase.auth().currentUser;
-        if (user) {
-            // user is logged in
-            var userRef = firebase.database().ref().child("users").child(user.uid);
-            // get the user's username
-            userRef.child("username").once("value").then(function(snapshot) {
-                currentUserName = snapshot.val();
-                var fileList = firebase.database().ref().child('files');
-                var newFile = fileList.push(); // generate a new fileID
-                newFile.set({
-                    'fileName': currentFileName,
-                    'fileContents': fileContents,
-                });
-                // add user to file's userList
-                newFile.child('userList').child(user.uid).set({'username' : currentUserName});
-                // add fileID to user's fileList
-                userRef.child('fileList').child(newFile.key).set({'fileName' : currentFileName});
-            });
-        }
-
-        // show the text in the editor
-        editor.textContent = fileContents;
+            // show the text in the editor
+            fileContents = data;
+            editor.textContent = data;
+        });
     });
-};
+
+    // Add file to user's account
+    var user = firebase.auth().currentUser;
+    if (user) {
+        // user is logged in
+        var userRef = firebase.database().ref().child("users").child(user.uid);
+        // get the user's username
+        userRef.child("username").once("value").then(function(snapshot) {
+            currentUserName = snapshot.val();
+            var fileList = firebase.database().ref().child('files');
+            var newFile = fileList.push(); // generate a new fileID
+            newFile.set({
+                'fileName': path,
+                'fileContents': fileContents,
+            });
+            // add user to file's userList
+            newFile.child('userList').child(user.uid).set({ 'username': currentUserName });
+            // add fileID to user's fileList
+            userRef.child('fileList').child(newFile.key).set({ 'fileName': path });
+        });
+    }
+
+});
 
 saveFileBtn.addEventListener('click', function() {
-    if (path === '') {
-        alert("You must open a file first.");
-        return;
+    if (path) {
+        fs.writeFile(path, editor.value, function(err) {
+            if (err) {
+                alert("An error ocurred updating the file" + err.message);
+                console.log(err);
+                return;
+            }
+            alert("The file has been succesfully saved");
+        });
+    } else {
+        alert("Please select a file first");
     }
-    var options = {
-        args: [path, editor.value]
-    };
-    PythonShell.run('./app/python_scripts/saveFile.py', options, function(err, results) {
-        if (err) throw err;
-    });
-});
+}, false);

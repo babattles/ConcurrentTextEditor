9039b132af3a430ba6b45a2c11c1b6acfa91282f
arunarjunakani <arunarjunakani@gmail.com>
2018-02-21 09:28:27 -0500

    Added functionality for switching between tabs

--- app/js/tab.js
+++ app/js/tab.js
@@ -7,14 +7,34 @@ $(document).ready(function(){
 });
 
 var addTab = function() {
-	tabs.push('New Tab' + (tabs.length==0?'':tabs.length));
+	tabs.push('Tab' + (tabs.length==0?'':tabs.length));
 	updateTabs();
 }
 
-var updateTabs = function () {
+var switchTab = function(args) {
+    var target = -1;
+    for (i in tabs) {
+    	if(tabs[i] == args.innerHTML) {
+    		target = i;
+    		break;
+    	}
+    }
+    if(target != -1) {
+    	updateTabs(target);
+    }
+}
+
+var updateTabs = function (activeTab) {
+	if(typeof activeTab == 'undefined') {
+		activeTab = tabs.length-1;
+	}
 	var tabHTML = '';
 	for (i in tabs) {
-		 tabHTML += '<td class="tab">' + tabs[i] + '</td>\n';
+		if(i == activeTab) {
+			tabHTML += '<div id="activeTab" class="tab rTableCell" onclick=\"switchTab(this)\">' + tabs[i] + '</div>\n';			
+		} else {
+			tabHTML += '<div class="tab rTableCell" onclick=\"switchTab(this)\">' + tabs[i] + '</div>\n';
+		}
 	}
 	$('#tabs').empty();
 	$('#tabs').append(tabHTML);

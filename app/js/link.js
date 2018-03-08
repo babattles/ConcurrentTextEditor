242355fc4cc433f22c3ac07f8f5ab7c63297823a
arunarjunakani <arunarjunakani@gmail.com>
2018-03-08 13:56:13 -0500

    Reads edits from Firebase

--- app/js/link.js
+++ app/js/link.js
@@ -1,1 +1,33 @@
-console.log(firebase);
+function getCurrentFile() {
+	let files = database.ref('files');
+	files.on('value', function(data) {
+		let file = data.val();
+		// console.log(file);
+	});
+}
+
+// var edits = ['George: Line #45', 'Arun: Line #154', 'Bryan: Line #221', 'Alex: Line #13', 'Paramesh: Line #191', 'Guangqi: Line #133'];
+
+function loadEdits() {
+	let editHTML = '';
+	let fileEdits = database.ref('files/-L73SCQ_U4ak-ETzrCzC/edits');
+	let userNames = database.ref('users');
+	userNames.on('value', function(data){
+		console.log(data.val());
+	});
+	fileEdits.on('value', function(data){
+		for(edit in data.val()) {
+			editVal = data.val()[edit];
+			if(editVal.type == 'add') {
+				editHTML += '<div id="edit-add" class="edit">' + editVal.content + '</div>\n';						
+			} else {
+				editHTML += '<div id="edit-remove" class="edit">' + editVal.content + '</div>\n';
+			}
+			// console.log('<div id="edit-add" class="edit">' + editVal.content + '</div>\n')
+		}
+		$('#edits').empty();
+		$('#edits').append(editHTML);	
+	});
+}
+
+loadEdits();

1db952d7aa5c1bde080d61369fde044af1e60a5c
Guangqi Sun <sun357@purdue.edu>
2018-03-17 18:37:42 -0400

    Tab functionality.

--- app/js/localFile.js
+++ app/js/localFile.js
@@ -14,7 +14,6 @@ var openFile = function() {
     dialog.showOpenDialog((fileNames) => {
         path = fileNames[0];
         currentFileName = fileNames[0].substring(fileNames[0].lastIndexOf(pathSeperator) + 1, fileNames[0].length);
-        addTab(currentFileName);
         fs.readFile(path, 'utf-8', (err, data) => {
             if (err) {
                 alert("An error ocurred reading the file :" + err.message);
@@ -32,6 +31,8 @@ var openFile = function() {
             // Show the text in ace editor. -1 specifies that cursor is at beginning of file.
             editor.setValue(data, -1);
 
+            //Create a new Tab for the file
+            addTab(currentFileName);
             // reset the state
             global_ignore = false;
 

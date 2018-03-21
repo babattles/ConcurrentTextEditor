7cb469c9c484b030224f2a4680d2ff210a7b895d
Paramesh Pradeep <parameshp@users.noreply.github.com>
2018-03-21 14:52:13 -0400

    incorporated concurrency in getEdits function.. testing done

--- app/js/concurrency.js
+++ app/js/concurrency.js
@@ -1,27 +1,10 @@
-var checkConcurrency = function() {
-    editRef.on("child_added", function(snapshot) {
-        var newEdit = snapshot.val();
-        updateFile(newEdit);
-    });
-    editRef.on("child_removed", function(snapshot) {
-        var newEdit = snapshot.val();
-        updateFile();
-    });
-    editRef.on("child_changed", function(snapshot) {
-        var newEdit = snapshot.val();
-        updateFile(newEdit);
-    });
-
+var checkConcurrency = function(edit) {
+    updateFile(edit);
 };
 var updateFile = function(edit) {
-    //var length = editRef.length;
-    /*for(i = 0; i < editRef.length; i++){
-        currentEdit = editRef[i];
-
-    }*/
     var editor = ace.edit("editor");
     var currentEdit = edit;
     var oldContents = editor.getSession().getValue();
-    var newContents = oldContents.slice(0, currentEdit.startIndex) + currentEdit + oldContents.slice(currentEdit.endIndex);
-    editor.getSession.setValue(newContents);
+    var newContents = oldContents.slice(0, currentEdit.startIndex) + currentEdit.content + oldContents.slice(currentEdit.endIndex);
+    editor.getSession().setValue(newContents);
 }

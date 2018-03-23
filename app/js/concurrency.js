5dac253a45da37bea5401ad8d23d273256602dd1
Alex Geier <alexjgeier@gmail.com>
2018-03-23 15:58:19 -0400

    Commented out setCursorPositionCode because it threw error

--- app/js/concurrency.js
+++ app/js/concurrency.js
@@ -7,10 +7,11 @@ var checkConcurrency = function (edit) {
 var updateFile = function (edit) {
     var editor = ace.edit("editor");
     var currentEdit = edit;
+    // var cursorPosition = editor.getCursorPosition();
     var oldContents = editor.getSession().getValue();
     var newContents = oldContents.slice(0, currentEdit.startIndex) + currentEdit.content + oldContents.slice(currentEdit.startIndex);
     global_ignore = true;
     editor.getSession().setValue(newContents);
-    editor.setCursorPosition(cursorPosition);
+    // editor.setCursorPosition(cursorPosition.row, cursorPosition.row);
     global_ignore = false;
 }

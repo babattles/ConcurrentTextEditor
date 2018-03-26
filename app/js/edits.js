9a9dbce9a83b02b03c36319e2bc5e8a568d9d010
arunarjunakani <arunarjunakani@gmail.com>
2018-03-26 09:02:42 -0400

    Theoretically implemented highlighting

--- app/js/edits.js
+++ app/js/edits.js
@@ -376,3 +376,96 @@ var getRowColumnIndices = function (characterIndex) {
 	var column = characterIndex - lastColumnIndices[row - 1] - 1;
 	return { row: row, column: column };
 };
+
+function loadEdits() {
+	if(currentKey == undefined) {
+		console.log('No File Selected');
+		return;
+	} 
+	$('#edits').empty();
+	let editHTML = '';
+	let fileEdits = database.ref('files/' + currentKey + '/edits');
+	let userNames = database.ref('users');
+	var parentList = [];
+	var childList = [];
+	userNames.on('value', function(userData){
+		fileEdits.on('value', function(data){
+			for(i in data.val()) {
+				if(!data.val()[i].parent) {
+					parentList.push({
+					 	'id': i,
+					 	'username': userData.val()[data.val()[i].user].username,
+					 	'content': data.val()[i].content,
+					 	'type': data.val()[i].type
+					});
+
+				} else {
+					childList.push({
+					 	'id': i,
+					 	'username': userData.val()[data.val()[i].user].username,
+					 	'content': data.val()[i].content,
+					 	'type': data.val()[i].type,
+					 	'parent': data.val()[i].parent
+					});
+				}
+			}
+			for(i in childList) {
+				for (j in parentList) {
+					if(parentList[j].id == childList[i].parent) {
+						parentList[j].child = childList[i];
+						break;
+					}
+				}
+			}
+			for(var i = 0; i < parentList.length; i++) {
+				editVal = parentList[i];
+				let eContent;
+				if(editVal.content.length > 20) {
+					eContent = editVal.content.substring(0, 20);
+				}
+				else {
+					eContent = editVal.content;
+				}
+				let divContent = '<b>' + editVal.username + '</b>: ' + eContent;
+				if(editVal.type == 'insert') {
+					editHTML += '<div id="edit-add" class="edit" onmouseover="editHover(\''
+					 + editVal.id + 
+					 '\')">' + divContent + '</div>\n';						
+				} else {
+					editHTML += '<div id="edit-remove" class="edit" onmouseover="editHover(\''
+					 + editVal.id + 
+					 '\')">' + divContent + '</div>\n';
+				}
+				if(editVal.child) {
+					childVal = editVal.child;
+					let childContent;
+					if(childVal.content.length > 20) {
+						childContent = childVal.content.substring(0, 20);
+					}
+					else {
+						childContent = childVal.content;
+					}
+					let childDiv = '<b>' + childVal.username + '</b>: ' + childContent;
+					if(childVal.type == 'insert') {
+						editHTML += '<div id="edit-add-child" class="edit" onmouseover="editHover(\'' + childVal.id + '\')">' + childDiv + '</div>\n';						
+					} else {
+						editHTML += '<div id="edit-remove-child" class="edit" onmouseover="editHover(\'' + childVal.id + '\')">' + childDiv + '</div>\n';
+					}
+				}
+			}
+			$('#edits').empty();
+			$('#edits').append(editHTML);
+			editHTML = '';	
+		});
+	});
+}
+
+function editHover(id) {
+	let hoveredEdit;
+	for(i in edits) {
+		if(edits[i].id == id) {
+			hoveredEdit = edits[i];
+		}
+	}
+	highlight(hoveredEdit);
+}

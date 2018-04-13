3476e82f475b658f2e14f52e68c78cf8da041c1e
babattles <babattles@gmail.com>
2018-04-13 16:46:09 -0400

    Add New Chat Channels + Load More Chat Messages
    Can now create new chat channels and click a button to load more chat messages in the current channel!

--- app/js/chatbox.js
+++ app/js/chatbox.js
@@ -15,59 +15,81 @@ $(document).ready(function () {
     });
 
     $("#channel").change(function () {
+        messageRef.off("child_added");
+        message_count = 0;
+        max_message_count = 10;
         loadMessages($(this).val());
+    });
+
+    $("#seeMore").click(function () {
+        messageRef.off("child_added");
+        max_message_count += 10;
+        message_count = 0;
+        loadMessages($("#channel").val());
+    });
+
+    $("#addChannel").click(function () {
+        var chat = document.getElementById("chat-container");
+        var btn = document.createElement("button");
+        var text = document.createElement("textarea");
+        btn.onclick = function () {
+            currentFile.child("messages").child("~" + text.value).push({
+                content: "Welcome to your new channel, " + text.value + "!",
+                user: "HiveText",
+            });
+            chat.removeChild(text);
+            chat.removeChild(btn);
+        };
+        btn.innerHTML = "Add New Channel";
+        chat.appendChild(text);
+        chat.appendChild(btn);
     });
 });
 
 var messageRef = null;
-var listeningForMessages = {};
+var message_count = 0;
+var max_message_count = 10;
 
 // Loads 10 most recent messages from database on file load
 var loadMessages = function (chan) {
+    var dd = document.getElementById("channel");
+    for (var i = 0; i < dd.options.length; i++) {
+        if (dd.options[i].text == chan) {
+            dd.options[i].selected = true;
+            break;
+        }
+    }
     messageRef = currentFile.child("messages").child(chan);
     var chat = document.getElementById("chat");
     while (chat.firstChild) {
         chat.removeChild(chat.firstChild);
     }
-    if (!listeningForMessages[messageRef]) {
-        messageRef.on("child_added", function (snapshot) {
-            var div = document.createElement("div");
-            var para = document.createElement("p");
-            var content = snapshot.val().content;
-            var message_user = snapshot.val().user;
-            var node = document.createTextNode(message_user + ": " + content);
-            para.appendChild(node);
-            div.appendChild(para);
-            chat.appendChild(div);
-        });
-        listeningForMessages[messageRef] = true;
-    } else {
-        messageRef.once("value", function (snapshot) {
-            snapshot.forEach(function (child) {
-                var div = document.createElement("div");
-                var para = document.createElement("p");
-                var content = child.val().content;
-                var message_user = child.val().user;
-                var node = document.createTextNode(message_user + ": " + content);
-                para.appendChild(node);
-                div.appendChild(para);
-                chat.appendChild(div);
-            })
-        });
-    }
+    messageRef.on("child_added", function (snapshot) {
+        if (message_count >= max_message_count && chat.firstChild) {
+            chat.removeChild(chat.firstChild);
+            message_count--;
+        }
+        var div = document.createElement("div");
+        var para = document.createElement("p");
+        var content = snapshot.val().content;
+        var message_user = snapshot.val().user;
+        var node = document.createTextNode(message_user + ": " + content);
+        para.appendChild(node);
+        div.appendChild(para);
+        chat.appendChild(div);
+        message_count++;
+    });
 }
 
 // Loads the channels
 var loadChannels = function () {
     $("#channel").empty();
     var dd = document.getElementById("channel");
-    currentFile.child("messages").once("value", function (snapshot) {
-        snapshot.forEach(function (child) {
-            var option = document.createElement("option");
-            option.text = child.key;
-            option.id = child.key;
-            dd.add(option);
-        });
+    currentFile.child("messages").on("child_added", function (child) {
+        var option = document.createElement("option");
+        option.text = child.key;
+        option.id = child.key;
+        dd.add(option);
     });
     loadMessages("General");
 }
